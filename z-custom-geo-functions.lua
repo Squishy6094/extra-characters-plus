6d71c82c047b62ee4ea72e0a9256500cb840304c
steven. <steven30042007@gmail.com>
2025-08-10 15:31:20 +0700

    Side mouth implementation ez pz

--- z-custom-geo-functions.lua
+++ z-custom-geo-functions.lua
@@ -15,25 +15,24 @@ local sSonicSpinDashActs = {
 
 local l = gLakituState
 
-function object_yaw_from_camera(o) -- Returns from 1 to 8 directions.
+function mario_yaw_from_camera(m) -- Returns from 1 to 8 directions.
     local tau = math.pi * 2
-    local pitch = object_pitch_from_camera(o)
 
-    local vector = {X = l.pos.x - o.oPosX, Y = l.pos.y - o.oPosY,  Z = l.pos.z - o.oPosZ}
-    local r0 = math.rad((o.oFaceAngleYaw * 360) / 0x10000)
+    local vector = {X = l.pos.x - m.pos.x, Y = l.pos.y - m.pos.y,  Z = l.pos.z - m.pos.z}
+    local r0 = math.rad((m.faceAngle.y * 360) / 0x10000)
     local r1 = r0 < 0 and tau - math.abs(r0) or r0
     local a0 = math.atan2(vector.Z, vector.X) + math.pi * 0.5
 
     local a1 = 0
-    if pitch == 2 or pitch == -2 then
+    --[[if pitch == 2 or pitch == -2 then
         a1 = math.rad((limit_angle(o.oFaceAngleYaw - l.yaw) * 360) / 0x10000)
-    else
-        a1 = ((a0 < 0 and tau - math.abs(a0) or a0) + r1)
-    end
+    else]]
+    a1 = ((a0 < 0 and tau - math.abs(a0) or a0) + r1)
+
     
 
     local a2 = (a1 % tau) * 8 / tau
-    local angle = (round(a2) % 8) + 1
+    local angle = (math.round(a2) % 8) + 1
     --djui_chat_message_create(tostring(angle))
     return angle
 end
@@ -68,6 +67,14 @@ end
 function geo_switch_mario_mouth_side(n)
     local switch = cast_graph_node(n)
     local m = geo_get_mario_state()
+    local angle = mario_yaw_from_camera(m)
+    
+    if angle <= 4 then
+        switch.selectedCase = 1
+    else
+        switch.selectedCase = 0
+    end
+    
 end
 
 -- Wapeach Axe Acts --
